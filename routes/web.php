9d455fc748a68557ddd334b5253b79ace1b8b5f1
CrushedKatana <charelsadewo123@gmail.com>
2025-02-18 11:19:16 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,31 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('/hello', function () {
+    return 'Hello World';
 
-Route::get('/about', function () {
-    return '2341720205';
-   
+// Route::get('/World', function () {
+//     return 'World';  
+
+// Route::get('/', function () {
+//     return 'Selamat Datang';
+
+// Route::get('/about', function () {
+//     return '2341720205 Charellino Kalingga Sadewo';
+
+// Route::get('/user/{name}', function ($name) {
+//     return 'Nama saya '.$name;  
+
+// Route::get('/articles/{id}', function ($id) {
+//     return 'Halaman Artikel dengan ID = ' . $id;
+
+// Route::get('/user/{name?}', function ($name=null) {
+//     return 'Nama saya '.$name;
+
+// Route::get('/user/{name?}', function ($name=null) {
+//     return 'Nama saya '.$name;
+
+// Route::get('/user/{name?}', function ($name='John') {
+// return 'Nama saya '.$name;
 });
+
